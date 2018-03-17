6ee018a28e6f10b43445395e1dc7db250c861b09
Tanoja Sunkam <sunka004@umn.edu>
2018-03-17 17:38:30 -0500

    changed how server works

--- 201802/01/MN/server.js
+++ 201802/01/MN/server.js
@@ -1,18 +1,26 @@
-'use strict';
-
 const express = require('express');
+const app = express();
+
+app.use('../MN', express.static((__dirname+'../MN')));
 
 // Constants
-const PORT = 8080;
+const PORT = 3000;
 //const HOST = '0.0.0.0';
 
-// App
-const app = express();
+//ROUTE
+// app.get('/', (req, res) => {
+//   res.send('Hello world\n');
+// });
+
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+	res.sendFile(__dirname+'/mn_attractions.html');
 });
+
+
 
 //app.listen(PORT, HOST);
 //console.log(`Running on http://${HOST}:${PORT}`);
 
-app.listen(3000);
+app.listen(PORT,function(){
+	console.log("Server running and listening in port " + PORT);
+});
